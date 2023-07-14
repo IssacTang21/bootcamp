a5ce5cb1e3d43f8365a44f4b23a3c1cf0bae87fa
Issac Tang <singtys0201@gmail.com>
2023-07-14 17:37:22 +0800

    Revised day5 exe6

--- bc-exercise/day5/JavaQuest6.java
+++ bc-exercise/day5/JavaQuest6.java
@@ -9,13 +9,23 @@ public class JavaQuest6 {
     // for loop to print first 15 numbers in Fibonacci Sequence
     int a = 1;
     int b = 1;
-    int sum = 2;
-    System.out.print(a + " " + b + " ");
-    for (int i = 3; i < 16; i++){
+    int sum = 0;
+    for (int i = 0; i < 15; i++) {
       sum = a + b;
-      System.out.print(sum + " ");
-      b = a;
-      a = sum;
+      System.out.print(a + " ");
+      a = b;
+      b = sum;
     }
+    // a = 1, b = 1, sum = 0, 
+    // a = 1, b = 1, sum = 2, 
+    // a = 1, b = 1, sum = 2, print 1
+    // a = 1, b = 1, sum = 2
+    // a = 1, b = 2, sum = 2
+    // a = 1, b = 2, sum = 3
+    // a = 1, b = 2, sum = 3, print 1
+    // a = 2, b = 2, sum = 3
+    // a = 2, b = 3, sum = 3
+    // a = 2, b = 3, sum = 5
+    // a = 2, b = 3, sum = 5, print 2 
   }
 }
